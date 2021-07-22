c29a37cd0ee71c4a07ec5dab1f2d7ab4d5194ed9
Eduardo Valle Gonçalves <e.valle23@outlook.com>
2021-07-22 20:58:06 -0300

    classe e pergutas do programa

---
+++ Abastecimento/AbastecimentoMotorista.cs
@@ -0,0 +1,43 @@
+﻿using System.Globalization;
+
+namespace Abastecimento
+{
+    class AbastecimentoMotorista
+    {
+        public string Nome { get; set; }
+        public double Litro { get; private set; }
+        public double Valor { get; private set; }
+        public int Hodometro { get; private set; }
+
+        public AbastecimentoMotorista(string nome, double litro, double valor, int hodometro)
+        {
+            Nome = nome;
+            Litro = litro;
+            Valor = valor;
+            Hodometro = hodometro;
+        }
+
+
+        public double ValorDoAbastecimento()
+        {
+            return Litro * Valor;
+        }
+
+        public override string ToString()
+        {
+            return Nome 
+                + ", Litros: " 
+                + Litro 
+                +", Valor por Litro: R$ " 
+                + Valor 
+                + ", Valor do Abastecimento: R$ " 
+                + ValorDoAbastecimento().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+
+
+
+
+    
+}
--- Abastecimento/Program.cs
+++ Abastecimento/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Abastecimento
 {
@@ -6,7 +7,45 @@ namespace Abastecimento
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            AbastecimentoMotorista dados;
+            double tA;
+
+            Console.WriteLine("CONTROLE DE ABASTECIMENTO E MEDIA");
+            Console.WriteLine();
+
+            Console.Write("Digite o nome do motorista: ");
+            string nome = Console.ReadLine();
+            Console.Write("Digite a placa do veiculos: ");
+            string placa = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.Write("Digite valor do Hodometro do ultimo abastecimento: ");
+            int ultimoHodometro = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.Write("Informe a quantidade do abastecimento: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++) {
+                Console.WriteLine("Digite os valores abaixo: ");
+                Console.WriteLine("Abastecimento: " + i + "°");
+                Console.Write("Litros: ");
+                double litros = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Valor por litro: ");
+                double valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Hodometro: ");
+                int hodometro = int.Parse(Console.ReadLine());
+                tA = litros * valor;
+                int km = hodometro - ultimoHodometro;
+                Console.WriteLine("Total do Abastecimento: ");
+                Console.WriteLine("Valor do Abastecimento: R$" + tA.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("Km rodados: "+km);
+            }
+
+            Console.WriteLine();
+
+            
+           
         }
     }
 }
